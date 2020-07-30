3eda1376492527da338f4f8a7dbcdf1ff3415aca
Pasquale Sacco <pasqsac@yahoo.it>
2020-07-30 10:05:29 +0200

    configured some packages (geolocation,firebase, push notification)

--- src/config/WebService.js
+++ src/config/WebService.js
@@ -1,17 +1,17 @@
-import OneSignal from "react-native-onesignal";
+import OneSignal from 'react-native-onesignal';
 
 const WebService = {
-  API_KEY_Google: "AIzaSyC3cg3CWrVwdNa1ULzzlxZ-gy-4gCp080M",
-  client_id: "VxZ2mhgm9iFfBeZAI2YsPbIcVEJXJSCBW7DCmNjl", // new production
+  API_KEY_Google: 'AIzaSyC3cg3CWrVwdNa1ULzzlxZ-gy-4gCp080M',
+  client_id: 'VxZ2mhgm9iFfBeZAI2YsPbIcVEJXJSCBW7DCmNjl', // new production
   client_secret:
-    "WCgzxHrllHBWtYMwKAkdgVVMpBwOJ1cXG5urlWfv3gVaBUlpHTbX0SCsLIjqVJM0eQyvlBPFGaaV5r0Elr1GTTT8P0jifZqcZceOS8qXZRXDB4zT1zQIxpYTRSbTaZQ9", // new production
-  tokenServer: "3a9e8cfb-bfc6-4a1f-b5c6-152b75a98688", // token necessario per autorizzare alcune richieste senza essere loggati,
+    'WCgzxHrllHBWtYMwKAkdgVVMpBwOJ1cXG5urlWfv3gVaBUlpHTbX0SCsLIjqVJM0eQyvlBPFGaaV5r0Elr1GTTT8P0jifZqcZceOS8qXZRXDB4zT1zQIxpYTRSbTaZQ9', // new production
+  tokenServer: '3a9e8cfb-bfc6-4a1f-b5c6-152b75a98688', // token necessario per autorizzare alcune richieste senza essere loggati,
 
-  url: "https://muvprod.tk", // new production
-  socket: "muvprod.tk",
-  OneSignalRestAPIKey: "MTk0MWFhMDctZjcwZi00Y2NhLWJmMjgtZDUwYjZkZDJkMGNj",
-  OneSignalAppId: "d8d9dde7-340e-4324-a4af-14235503cafb",
-  BugsnagAppId: "f2831bdc190a0175710fcdebcf5ba4f3",
+  url: 'https://muvprod.tk', // new production
+  socket: 'muvprod.tk',
+  OneSignalRestAPIKey: 'MTk0MWFhMDctZjcwZi00Y2NhLWJmMjgtZDUwYjZkZDJkMGNj',
+  OneSignalAppId: 'd8d9dde7-340e-4324-a4af-14235503cafb',
+  BugsnagAppId: 'f2831bdc190a0175710fcdebcf5ba4f3',
 
   // url: "https://muvdev.tk", // new test
   // socket: "muvdev.tk",
