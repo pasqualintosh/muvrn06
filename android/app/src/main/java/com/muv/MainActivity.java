6a64b9e93d1feef428b1a0f2535625dd24dec330
Pasquale Sacco <pasqsac@yahoo.it>
2020-08-03 18:30:04 +0200

    chanded project name 4 android

--- android/app/src/main/java/com/muv/MainActivity.java
+++ android/app/src/main/java/com/muv/MainActivity.java
@@ -1,4 +1,4 @@
-package com.muvrn06;
+package com.muv;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "muvrn06";
+    return "muv";
   }
 }
--- android/app/src/main/java/com/muv/MainApplication.java
+++ android/app/src/main/java/com/muv/MainApplication.java
@@ -1,4 +1,4 @@
-package com.muvrn06;
+package com.muv;
 
 import android.app.Application;
 import android.content.Context;
@@ -63,7 +63,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.muvrn06.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.muv.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
